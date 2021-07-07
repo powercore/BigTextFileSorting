fe300235d528f47dcab04336586b0d5fc23e9216
powercore <shimyakov@gmail.com>
2021-07-07 09:55:09 +0500

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BigTextFileSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
